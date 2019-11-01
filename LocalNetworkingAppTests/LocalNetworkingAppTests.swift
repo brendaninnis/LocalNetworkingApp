c73fd0e1620ef00c6daa3238408a69406ebafdfe
Brendan Innis <brendan@wholepunk.com>
2019-11-01 15:12:22 -0700

    Initial Commit

---
+++ LocalNetworkingAppTests/LocalNetworkingAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  LocalNetworkingAppTests.swift
+//  LocalNetworkingAppTests
+//
+//  Created by Brendan Innis on 2019-11-01.
+//  Copyright © 2019 Brendan Innis. All rights reserved.
+//
+
+import XCTest
+@testable import LocalNetworkingApp
+
+class LocalNetworkingAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
