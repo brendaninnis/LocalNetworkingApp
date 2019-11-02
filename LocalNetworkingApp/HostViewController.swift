3819b7c262691270c75ee9091d2791576a973d4f
Brendan Innis <brendan@wholepunk.com>
2019-11-01 19:32:54 -0700

    merged client and host view controllers

--- LocalNetworkingApp/HostViewController.swift
+++ LocalNetworkingApp/HostViewController.swift
@@ -11,45 +11,130 @@ import CocoaAsyncSocket
 
 class HostViewController: UIViewController {
 
-    var netService: NetService!
-    var socket: GCDAsyncSocket!
+    var netService: NetService?
+    var socket: GCDAsyncSocket?
     var connectedSockets: [GCDAsyncSocket] = []
+    var host = false
+    var connected = false
+    var netServiceBrowser: NetServiceBrowser?
+    var serverAddresses: [Data]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        startNetServiceBrowser()
+    }
+    
+    func startNetServiceBrowser() {
+        netServiceBrowser = NetServiceBrowser()
+        netServiceBrowser?.delegate = self
+        netServiceBrowser?.searchForServices(ofType: "_LocalNetworkingApp._tcp.", inDomain: "local.")
+    }
+    
+    func stopNetServiceBrowser() {
+        
+    }
+
+    func connectToNextAddress() {
+        var done = false
+        while (!done && serverAddresses?.count ?? 0 > 0) {
+            if let addr = serverAddresses?.remove(at: 0) {
+                do {
+                    try socket?.connect(toAddress: addr)
+                    done = true
+                } catch let error {
+                    print("ERROR: \(error)")
+                }
+            }
+        }
+        
+        if !done {
+            print("Unable to connect to any resolved address")
+        }
+    }
+    
+    func hostNetService() {
+        host = true
+        
         socket = GCDAsyncSocket(delegate: self, delegateQueue: DispatchQueue.main)
         do {
-            try socket.accept(onPort: 0)
-            let port = socket.localPort
+            try socket?.accept(onPort: 0)
+            let port = socket!.localPort
             
             netService = NetService(domain: "local.", type: "_LocalNetworkingApp._tcp.", name: "Host Device", port: Int32(port))
-            netService.delegate = self
-            netService.publish()
+            netService?.delegate = self
+            netService?.publish()
         } catch let error {
             print("ERROR: \(error)")
         }
     }
 }
 
-extension HostViewController: GCDAsyncSocketDelegate {
-    func socket(_ sock: GCDAsyncSocket, didAcceptNewSocket newSocket: GCDAsyncSocket) {
-        connectedSockets.append(newSocket)
+extension HostViewController: NetServiceBrowserDelegate {
+    func netServiceBrowser(_ browser: NetServiceBrowser, didNotSearch errorDict: [String : NSNumber]) {
+        print("ERROR: \(errorDict)")
     }
     
-    func socketDidDisconnect(_ sock: GCDAsyncSocket, withError err: Error?) {
-        if let index = connectedSockets.firstIndex(of: sock) {
-            connectedSockets.remove(at: index)
+    func netServiceBrowser(_ browser: NetServiceBrowser, didFind service: NetService, moreComing: Bool) {
+        if netService == nil {
+            netService = service
+            netService?.delegate = self
+            netService?.resolve(withTimeout: 5)
         }
+    }
+    
+    func netServiceBrowserDidStopSearch(_ browser: NetServiceBrowser) {
+        print("NetServiceBrowser did stop search")
     }
 }
 
 extension HostViewController: NetServiceDelegate {
+    
+    // Client
+    func netService(_ sender: NetService, didNotResolve errorDict: [String : NSNumber]) {
+        print("NetService did not resolve: \(errorDict)")
+    }
+    
+    func netServiceDidResolveAddress(_ sender: NetService) {
+        if serverAddresses == nil {
+            serverAddresses = sender.addresses
+        }
+        if socket == nil {
+            socket = GCDAsyncSocket(delegate: self, delegateQueue: DispatchQueue.main)
+            connectToNextAddress()
+        }
+    }
+    
+    // Host
     func netServiceDidPublish(_ sender: NetService) {
         print("Bonjour Service Published: domain(\(sender.domain)) type(\(sender.type)) name(\(sender.name)) port(\(sender.port))")
     }
     
     func netService(_ sender: NetService, didNotPublish errorDict: [String : NSNumber]) {
         print("Failed to publish Bonjour Service domain(\(sender.domain)) type(\(sender.type)) name(\(sender.name))\n\(errorDict)")
+    }
+}
+
+extension HostViewController: GCDAsyncSocketDelegate {
+    func socket(_ sock: GCDAsyncSocket, didConnectToHost host: String, port: UInt16) {
+        print("Socket did connect to host \(host) on port \(port)")
+        connected = true
+    }
+    
+    func socket(_ sock: GCDAsyncSocket, didAcceptNewSocket newSocket: GCDAsyncSocket) {
+        connectedSockets.append(newSocket)
+    }
+    
+    func socketDidDisconnect(_ sock: GCDAsyncSocket, withError err: Error?) {
+        print("Socket did disconnect \(err?.localizedDescription ?? "")")
+        if host {
+            if let index = connectedSockets.firstIndex(of: sock) {
+                connectedSockets.remove(at: index)
+            }
+        } else {
+            if !connected {
+                connectToNextAddress()
+            }
+        }
     }
 }
